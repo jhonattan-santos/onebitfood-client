a116348640bd2ca52fab99e1677780ce6aae06de
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2021-05-18 22:48:25 -0300

    Included into home component the basic tags and settings

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
-import Container from 'react-bootstrap/Container'
+import { Button, Row, Col } from 'react-bootstrap';
+import Link from 'next/link';
+import { FaCrosshairs } from 'react-icons/fa';
 
 export default function Home() {
   return (
-  <>
-    <Container>
-      Hi, dear!
-    </Container>
-  </>
+    <>
+      <Row className="mt-8 justify-content-center">
+        <Col md={7} xs={12} className="text-center">
+          <h1 className="fw-bolder text-custom-gray-darker mb-5 lh-base-1 display-5">
+            Comida saudável e gostosa direto na sua casa
+          </h1>
+          <Link href="/restaurant s">
+            <Button variant="custom-red" size="lg" className="text-white">
+              <FaCrosshairs className="pb-1"></FaCrosshairs>
+              <span className="px-2 fw-bolder">Encontrar Agora</span>
+            </Button>
+          </Link>
+        </Col>
+      </Row>
+    </>
   );
 }
