588687185a5f87660293af93f7bc38043f2348ad
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2021-05-18 23:59:07 -0300

    Added cards of restaurants

--- components/ListRestaurants/index.js
+++ components/ListRestaurants/index.js
@@ -1,14 +1,52 @@
 import { Row, Col } from 'react-bootstrap';
+import Restaurants from '../../pages/restaurants';
+import Restaurant from './Restaurant';
 
 export default function ListRestaurants() {
+  const restaurants = [
+    {
+      id: 1,
+      name: 'example 1',
+      description: 'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
+      delivery_tax: '5',
+      image_url: '/restaurant.jpeg',
+      category_title: 'Cozinha japonesa',
+    },
+    {
+      id: 2,
+      name: 'example 2',
+      delivery_tax: '10',
+      description: 'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
+      image_url: '/restaurant.jpeg',
+      category_title: 'Cozinha mineira',
+    },
+    {
+      id: 3,
+      name: 'example 3',
+      delivery_tax: '15',
+      description: 'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
+      image_url: '/restaurant.jpeg',
+      category_title: 'Cozinha vegana',
+    },
+    {
+      id: 4,
+      name: 'example 4',
+      delivery_tax: '10',
+      description:
+        'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
+      image_url: '/restaurant.jpeg',
+      category_title: 'Cozinha vegana',
+    },
+  ];
+
   return (
-    <>
-      <h3>Restaurantes</h3>
+    <div className="mt-5">
+      <h3 className="fw-bolder">Restaurantes</h3>
       <Row>
-        <Col>
-          <p>restaurantes...</p>
-        </Col>
+        {restaurants.map((restaurant, i) => (
+          <Restaurant {...restaurant} key={i} />
+        ))}
       </Row>
-    </>
+    </div>
   );
 }
