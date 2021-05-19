d7a202621c11e91d6770eee0c8c8b7f4fc8d4756
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2021-05-18 21:38:31 -0300

    Added theme settings and import fonts to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
-import Head from "next/head"
-import "../styles/globals.css"
+import Head from "next/head";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>OnebitFood v2</title>
+        <link ref="icon" href="/favicon.icon" />
       </Head>
       <main>
         <Component {...pageProps} />
       </main>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
