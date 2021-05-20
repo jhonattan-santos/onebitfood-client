a888ad9e8d43d66c0a901b109228aa7b4e91d370
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2021-05-20 07:58:37 -0300

    Added callback components and spinner when not loaded restaurants

--- components/ListRestaurants/index.js
+++ components/ListRestaurants/index.js
@@ -1,22 +1,36 @@
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Spinner, Alert } from 'react-bootstrap';
 import Restaurant from './Restaurant';
 import getRestaurants from '../../services/getRestaurants';
 
 export default function ListRestaurants() {
   const { restaurants, isLoading, isError } = getRestaurants();
-  console.log(restaurants);
-  if (!restaurants) {
-    return null;
+
+  function renderContent() {
+    if (isError) {
+      return (
+        <Col>
+          <Alert variant="custom-red"></Alert>
+        </Col>
+      );
+    } else if (isLoading) {
+      return (
+        <Col>
+          <Spinner animation="border" />
+        </Col>
+      );
+    } else if (restaurants.length === 0) {
+      return <Col>Nenhum restaurante disponível ainda...</Col>;
+    } else {
+      return restaurants.map((restaurant, i) => (
+        <Restaurant {...restaurant} key={i} />
+      ));
+    }
   }
 
   return (
     <div className="mt-5">
       <h3 className="fw-bolder">Restaurantes</h3>
-      <Row>
-        {restaurants.map((restaurant, i) => (
-          <Restaurant {...restaurant} key={i} />
-        ))}
-      </Row>
+      <Row>{renderContent()}</Row>
     </div>
   );
 }
