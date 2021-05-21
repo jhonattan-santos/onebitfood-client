e08b0d5600a8f15b4cee51a7a145c153a2228d3f
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2021-05-21 00:38:26 -0300

    Added address form and started to use state

---
+++ components/AddressModal/index.js
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import { Modal } from 'react-bootstrap';
+import FormAddress from './FormAddress';
+
+import { useRecoilState } from 'recoil';
+import addressState from '../../store/atoms/addressAtom';
+import { useRouter } from 'next/router';
+
+export default function AddressModal(props) {
+  const [address, setAddress] = useRecoilState(addressState);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.asPath != '/' && address.city == '') {
+      props.onShow();
+    }
+  }, [router]);
+
+  return (
+    <Modal
+      show={props.show}
+      size="sm"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header>
+        <h5 className="fw-bold mt-2">Endereço de Entrega</h5>
+      </Modal.Header>
+      <Modal.Body>
+        <FormAddress
+          onHide={() => props.onHide()}
+          onShow={() => props.onShow()}
+        />
+      </Modal.Body>
+    </Modal>
+  );
+}
