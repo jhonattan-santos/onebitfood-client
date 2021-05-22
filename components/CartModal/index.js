36412218b329d504adac4e930c5620e54424c4fe
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2021-05-22 12:59:59 -0300

    Added a cart data to modal

--- components/CartModal/index.js
+++ components/CartModal/index.js
@@ -1,7 +1,14 @@
+import Link from 'next/link';
 import { Modal } from 'react-bootstrap';
 import Cart from '../Cart';
+import { Button } from 'react-bootstrap';
+
+import cartState from '../../store/atoms/cartAtom';
+import { useRecoilState } from 'recoil';
 
 export default function CartModal(props) {
+  const [cart] = useRecoilState(cartState);
+
   return (
     <Modal
       show={props.show}
@@ -16,6 +23,15 @@ export default function CartModal(props) {
       </Modal.Header>
       <Modal.Body>
         <Cart show={props.show} />
+        {cart.products.length > 0 && (
+          <div className="text-center pt-2">
+            <Link href="/orders/new">
+              <Button variant="custom-red" className="text-white">
+                Finalizar pedido
+              </Button>
+            </Link>
+          </div>
+        )}
       </Modal.Body>
     </Modal>
   );
